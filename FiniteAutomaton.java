36367ec8d2afcfa32ff2ff62b6cc68dce2930ab1
Kongphop Worawutkasem <del2547.pv@gmail.com>
2024-03-29 16:19:31 +0700

    test commit

--- FiniteAutomaton.java
+++ FiniteAutomaton.java
@@ -2,6 +2,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class FiniteAutomaton {
     private int state;
     private int[][] table = {{1, 0}, {1, 2}, {1, 0}, {3, 0}, {1, 4}, {5, 0}};
--- Menu.java
+++ Menu.java
@@ -1,6 +1,7 @@
 import java.lang.reflect.Array;
 import java.util.Scanner;
 
+
 public class Menu {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
