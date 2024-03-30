f395029c28ae84d12508754e34cd2e7d42f774e1
Deler <del2547.pv@gmail.com>
2024-03-30 22:17:40 +0700

    complete FiniteAutomaton.java

--- FiniteAutomaton.java
+++ FiniteAutomaton.java
@@ -1,47 +1,55 @@
-
 import java.util.Arrays;
 import java.util.Scanner;
-
-
 public class FiniteAutomaton {
-    private int state;
-    private int[][] table = {{1, 0}, {1, 2}, {1, 0}, {3, 0}, {1, 4}, {5, 0}};
-    private int[] acceptState = {4};
-
+    private String [] inputState;
+    private String[][] graph = {
+        {"1", "0", "-1"}, //s0
+        {"-1", "0", "1"}, //s1
+        {"1", "0", "-1"}, //s2
+    };
+    private boolean acceptState = false;
     public FiniteAutomaton(){
-        this.state = 0;
         homeProgram();
     }
     public boolean accept(String input) {
-        for (char symbol : input.toCharArray()) {
-            char [] list = input.toCharArray();
-            System.out.println("in for loop " + Arrays.toString(list));
-            int inputIndex;
-            if (symbol == '0') {
-                inputIndex = 0;
-            } 
-            else if (symbol == '1') {
-                inputIndex = 1;
-            } 
-            else {
-                return false;
+        inputState = input.split(" ");
+        int root = 0; // s1
+        while (acceptState == false){
+            for (int i = 0; i < inputState.length; i++){
+                for (int j = 0; j < graph[root].length; j++){
+                    if ((root == 1) && (i == inputState.length - 1)){
+                        if (inputState[i].equals(graph[root][j])){
+                            root = j;
+                            if (root == 2){
+                                acceptState = true;
+                                break;
+                            }
+                        }
+                    }
+                    else if (inputState[i].equals(graph[root][j])){
+                        acceptState = false;
+                        root = j;
+                        break;
+                    }
+                }
             }
-            state = table[state][inputIndex];
-            System.out.println("state: " + state);
-            
-        }
-        for (int acceptingState : acceptState) {
-            if (state == acceptingState) {
+            if (acceptState){
                 return true;
             }
+            else{
+                return false;
+            }
         }
-        System.out.println("check accetpt");   
         return false;
     }
     public void homeProgram() {
         Scanner sc = new Scanner(System.in);
         System.out.println("-----Finite Automaton Program------");
-        System.out.println("*Enter the set with 0 and 1 and enter 9 to stop*");
+        System.out.println("Graph(From slide DM10 P.14): ");
+        for (String[] arr : graph){
+            System.out.println(Arrays.toString(arr));
+        }
+        System.out.println("*Enter the set with 0 and 1 and white space between letters*");
         System.out.print("Enter set: ");
         String input = sc.nextLine();
         if (accept(input)){
